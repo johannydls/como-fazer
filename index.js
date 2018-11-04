1bbef9ab6babb6bbf097d56a4431cccaab8fea59
Johanny de Lucena Santos <johanny.dls@gmail.com>
2018-11-04 13:54:40 -0300

    POST e DELETE de categorias no banco de dados do Firebase. Uso do axios para lidar com a API

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded());
 
 const port = process.env.PORT || 3000;
 
-let i = 0;
-
-app.get('/', (request, response) => {
+app.get('/', async (request, response) => {
     console.log("[NEW REQUEST]: GET / 200 OK");
-    i++
-    response.render('index', {i: i});
+    const content = await axios.get('https://jls-como-fazer-devpleno.firebaseio.com/teste.json');
+
+    response.render('index', {
+        i: content.data
+    });
 });
+
+app.get('/categorias/nova', (req, res) => {
+    res.render('categorias/nova')
+});
+
+app.post('/categorias/nova', async (req, res) => {
+
+    await axios.post('https://jls-como-fazer-devpleno.firebaseio.com/categorias.json', {
+        categoria: req.body.categoria
+    });
+
+    res.redirect('/categorias');
+})
+
+app.get('/categorias', async (req, res) => {
+    const content = await axios.get('https://jls-como-fazer-devpleno.firebaseio.com/categorias.json');
+    if (content.data) {
+        const categorias = Object
+            .keys(content.data)
+            .map(key => {
+                return {
+                    id: key,
+                    ...content.data[key]
+                }
+            });
+
+        res.render('categorias/index', {
+            categorias: categorias
+        });
+    } else {
+        res.render('categorias/index', {
+            categorias: []
+        });
+    }
+});
+
+app.get('/categorias/excluir/:id', async (req, res) => {
+    await axios.delete(`https://jls-como-fazer-devpleno.firebaseio.com/categorias/${req.params.id}.json`);
+    res.redirect('/categorias');
+})
 
 app.listen(port, (err) => {
     if (err) console.log("Erro:", err);
