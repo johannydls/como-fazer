a9eb33c4875e55920d48830db4eb0ef3418cd74a
Johanny de Lucena Santos <johanny.dls@gmail.com>
2018-11-04 15:09:17 -0300

    Refatoração. Tirando funções de rotas para controllers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const api = require('./api');
+const categorias = require('./routes/categorias');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded());
@@ -11,43 +11,11 @@ const port = process.env.PORT || 3000;
 
 app.get('/', async (request, response) => {
     console.log("[NEW REQUEST]: GET / 200 OK");
-    const content = await api.list('teste');
-    response.render('index', {
-        i: content.data
-    });
+    const content = await axios.get('https://jls-como-fazer-devpleno.firebaseio.com/teste.json');
+    response.render('index', { i: content.data });
 });
 
-app.get('/categorias/nova', (req, res) => {
-    res.render('categorias/nova')
-});
-
-app.post('/categorias/nova', async (req, res) => {
-    await api.create('categorias', { categoria: req.body.categoria });
-    res.redirect('/categorias');
-})
-
-app.get('/categorias', async (req, res) => {
-    const categorias = await api.list('categorias');
-    res.render('categorias/index', { categorias });
-});
-
-app.get('/categorias/excluir/:id', async (req, res) => {
-    await api.apagar('categorias', req.params.id);
-    res.redirect('/categorias');
-})
-
-
-app.get('/categorias/editar/:id', async(req, res) => {
-    const categoria = await api.get('categorias', req.params.id);
-    res.render('categorias/editar', { categoria });
-});
-
-app.post('/categorias/editar/:id', async (req, res) => {
-    await api.update('categorias', req.params.id, {
-        categoria: req.body.categoria
-    });
-    res.redirect('/categorias');
-})
+app.use('/categorias', categorias);
 
 app.listen(port, (err) => {
     if (err) console.log("Erro:", err);
